6827938dcf01c56c0064d0c0bc4e109cca2660da
Sergey F <Chukak12@gmail.com>
2018-04-14 19:03:33 +0300

    fix brackets

--- sorting/bubble.py
+++ sorting/bubble.py
@@ -5,4 +5,4 @@ def bubble_sorting(array):
     for i in range(len(array)):
         for j in range(len(array) - 1, i, -1):
             if array[j-1] > array[j]:
-                array[j], array[j-1] = array[j-1, array[j]]
+                array[j], array[j-1] = array[j-1], array[j]
